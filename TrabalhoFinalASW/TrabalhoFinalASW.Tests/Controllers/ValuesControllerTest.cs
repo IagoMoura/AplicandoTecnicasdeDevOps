dcb8740d0309e983aa8c43ec9da6fea1db6c63d2
Iago Rayner Moura <emaildoiagoo@gmail.com>
2015-08-18 21:35:03 -0300

    Teste

--- TrabalhoFinalASW/TrabalhoFinalASW.Tests/Controllers/ValuesControllerTest.cs
+++ TrabalhoFinalASW/TrabalhoFinalASW.Tests/Controllers/ValuesControllerTest.cs
@@ -39,7 +39,7 @@ namespace TrabalhoFinalASW.Tests.Controllers
             string result = controller.Get(5);
 
             // Assert
-            Assert.AreEqual("value1", result);
+            Assert.AreEqual("value2", result);
         }
 
         [TestMethod]
