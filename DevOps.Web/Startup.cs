f856c7e3f0d30f0bf9031dccd29c9aa3bc53b11a
Iago Rayner Moura <emaildoiagoo@gmail.com>
2016-08-04 17:09:49 -0300

    Novo Projeto

---
+++ DevOps.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevOps.Web.Startup))]
+namespace DevOps.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
