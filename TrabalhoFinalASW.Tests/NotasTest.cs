10e12c1ea954a8ff4e475d6b102016f0692798f8
Iago Rayner Moura <emaildoiagoo@gmail.com>
2016-08-04 17:18:24 -0300

    Teste Unitário irá falhar.

--- TrabalhoFinalASW.Tests/NotasTest.cs
+++ TrabalhoFinalASW.Tests/NotasTest.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TrabalhoFinalASW.Controllers;
 using System.Web.Http.Results;
 using System.Collections.Generic;
 
@@ -12,17 +11,7 @@ namespace TrabalhoFinalASW.Tests
         [TestMethod]
         public void GET()
         {
-            //// Arrange
-            NotasController controller = new NotasController();
-
-            //// Act
-            var result = controller.Get();
-            //ViewResult result = controller.Index() as ViewResult;
-
-            //// Assert
-            Assert.IsInstanceOfType(result, typeof(OkNegotiatedContentResult<List<TrabalhoFinalASW.Controllers.Notas>>));
-
-            Assert.IsTrue(1 == 1, "Um não é igual a um");
+            Assert.IsTrue(1 == 2, "Um não é igual a um");
         }
     }
 }
